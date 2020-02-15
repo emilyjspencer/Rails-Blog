42255fe4fa13f903425d6043c7d1f14136c725c7
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-15 19:51:59 +0000

    Timestamps added to articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_185029) do
+ActiveRecord::Schema.define(version: 2020_02_15_190847) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
