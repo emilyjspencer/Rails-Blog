550293c8f1cc41f88f3b91913c52c23df1a34d69
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-15 15:09:32 +0000

    Refactors Article Controller actions to make code more DRY by using a before filter

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController # all controllers inherit from ApplicationController
-  
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def new
     @article = Article.new
   end
@@ -15,15 +16,12 @@ class ArticlesController < ApplicationController # all controllers inherit from 
    end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end 
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
      flash[:notice] = "Article was updated"
      redirect_to article_path(@article)
@@ -38,7 +36,6 @@ class ArticlesController < ApplicationController # all controllers inherit from 
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was deleted"
     redirect_to articles_path
@@ -50,5 +47,9 @@ class ArticlesController < ApplicationController # all controllers inherit from 
   def article_params
     params.require(:article).permit(:title, :description)
   end
+
+  def set_article
+    @article = Article.find(params[:id])
+ end
 
 end
