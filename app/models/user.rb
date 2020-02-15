59761757fb2e7c55b496bcded863879d842d1e58
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-15 19:03:03 +0000

    Adds validation to ensure that each article is saved with the user who wrote it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :articles # add an association between users and articles
+  validates :user_id, presence: true
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 80 }, uniqueness: { case_sensitive: false }, 
